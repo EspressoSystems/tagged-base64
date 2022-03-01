0651a3f6e9f82252cf75c9330d49ff6a078936a0
Joe Doyle <joe@translucence.net>
2022-02-28 16:15:47 -0800

    Remove wasm-server.py

--- run-js-test.sh
+++ run-js-test.sh
@@ -4,5 +4,5 @@ cargo build --release --target wasm32-unknown-unknown
 mkdir -p js-test/tagged-base64
 wasm-bindgen --omit-imports --target web target/wasm32-unknown-unknown/release/tagged-base64.wasm --out-dir js-test/tagged-base64
 pushd js-test
-exec python ../wasm-server.py
+exec python -m http.server
 
